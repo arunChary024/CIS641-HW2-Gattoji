48d2cf5c2240200b6fb978bd2e4cabf7ab589fc8
arunChary024 <gattojia@mail.gvsu.edu>
2023-09-27 23:43:29 -0400

    Changes

--- script.js
+++ script.js
@@ -3,7 +3,3 @@
 document.getElementById('projectButton').addEventListener('click', function () {
     window.location.href = 'project.html';
 });
-
-document.getElementById('exitButton').addEventListener('click', function () {
-    window.location.href = 'index.html';
-});
