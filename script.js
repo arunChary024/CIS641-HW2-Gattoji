9a497bef750e5600be6a9fd448daa4de917d44cf
arunChary024 <gattojia@mail.gvsu.edu>
2023-09-27 23:23:11 -0400

    Adding Website Code

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('projectButton').addEventListener('click', function () {
+    window.location.href = 'project.html';
+});
+
+document.getElementById('exitButton').addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
